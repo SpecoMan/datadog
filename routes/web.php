b84d7dd403196238e6c3b34d5dedcba2e0be3b24
Mantas Gelčius <manciusg@gmail.com>
2017-07-03 23:19:07 +0300

    Updated routes
    Added route group for vehicles

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,7 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::group(['prefix' => 'vehicles', 'middleware' => 'admin'], function(){
+    Route::get('/', ['as' => 'vehicles', 'uses' => 'VehiclesController@index']);
+});
