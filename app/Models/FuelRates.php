c9351882e06d1bc2730a3ee3d0de54f06ea0228c
Mantas Gelčius <manciusg@gmail.com>
2017-07-04 15:20:01 +0300

    Updated FuelRates model
    Added necessary functions and created vehicle() function to parse relevent data from connections table

--- app/Models/FuelRates.php
+++ app/Models/FuelRates.php
@@ -6,5 +6,25 @@ use Illuminate\Database\Eloquent\Model;
 
 class FuelRates extends Model
 {
-    //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'idle', 'going', 'unloading'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+    ];
+
+    public function vehicle(){
+        $connectionsTable = (new VehiclesFuelRatesConnections())->getTable();
+       return $this->belongsToMany(Vehicles::class, $connectionsTable, 'fuel_rate_id','vehicle_id');
+    }
 }
