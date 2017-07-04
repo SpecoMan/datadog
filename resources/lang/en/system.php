ceaddc3f4bb9a5c6de22211e38350d020c719ffa
Mantas Gelčius <manciusg@gmail.com>
2017-07-04 19:41:44 +0300

    updated translations

--- resources/lang/en/system.php
+++ resources/lang/en/system.php
@@ -3,5 +3,19 @@
 return [
 
     'vehicles' => 'Vehicles',
+    'vehicles.enter.name' => 'Vehicle name',
+    'fuel_rates' => 'Fuel rates',
+    'create' => 'Create',
+    'edit' => 'Edit',
+    'date' => 'Date',
+    'destination' => 'Destination route',
+    'speedometer_start' => 'Speedometer at beginning',
+    'speedometer_end' => 'Speedometer at end',
+    'time_left_terminal' => 'Time when left terminal',
+    'time_enter_client' => 'Time when arrived to client',
+    'unload_time' => 'Unloading time',
+    'time_left_client' => 'Time left client',
+    'time_enter_terminal' => 'Time when arrived to terminal',
+    'travel_data' => 'Travel data'
 
 ];
