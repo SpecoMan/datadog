c6329904a2b061b5f75ca0a0ef5947fd7999d539
Mantas Gelčius <manciusg@gmail.com>
2017-07-04 23:15:45 +0300

    Added commenting to Fuel rates Controller

--- app/Http/Controllers/FuelRatesController.php
+++ app/Http/Controllers/FuelRatesController.php
@@ -9,19 +9,29 @@ use Illuminate\Http\Request;
 
 class FuelRatesController extends Controller
 {
+    /**
+     * Function to show sorted vehicles array with fuel rates
+     * if it has
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function index()
     {
         $vehiclesRecords = Vehicles::get()->toArray();
         $fuelRecords = FuelRates::all();
 
         $vehiclesArray = [];
+
         if (isset($fuelRecords->toArray()[0])) {
             foreach ($vehiclesRecords as $key => $vehiclesRecord)
                 foreach ($fuelRecords as $fuelRecord)
                     if ($fuelRecord->vehicle()->get()->toArray()['0']['id'] == $vehiclesRecord['id']) {
+
                         $vehiclesArray[$key] = $vehiclesRecord;
                         $vehiclesArray[$key][0] = $fuelRecord->toArray();
+
                     } elseif (!isset($vehiclesArray[$key]['0'])) {
+
                         $vehiclesArray[$key] = $vehiclesRecord;
                     }
         } else
@@ -30,6 +40,12 @@ class FuelRatesController extends Controller
         return view('system.fuel_rates', ['vehiclesArray' => $vehiclesArray]);
     }
 
+    /**
+     * Function to create fuel rates for vehicle
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function create(Request $request)
     {
         $record = new FuelRates([
@@ -52,6 +68,12 @@ class FuelRatesController extends Controller
     }
 
 
+    /**
+     * Function to fuel rates for vehicle
+     *
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse
+     */
     public function edit(Request $request)
     {
         FuelRates::where('id', $request->toArray()['id'])
