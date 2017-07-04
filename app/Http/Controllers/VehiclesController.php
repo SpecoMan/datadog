07f88316cf0ca8c6ed742b0f1a80637dd82f2446
Mantas Gelčius <manciusg@gmail.com>
2017-07-04 10:52:07 +0300

    Updated Vehicles Controller
    Updated index() function:
    Now it returns vehicles array to blade
    Added new functions:
    create() - for creating vehicle
    edit() - for editing vehicle
    delete() - for deleting vehicle

--- app/Http/Controllers/VehiclesController.php
+++ app/Http/Controllers/VehiclesController.php
@@ -2,11 +2,40 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Vehicles;
 use Illuminate\Http\Request;
 
 class VehiclesController extends Controller
 {
-    public function index(){
-        return view('system.vehicles');
+    public function index()
+    {
+        return view('system.vehicles', ['records' => Vehicles::get()->toArray()]);
+    }
+
+    public function create(Request $request)
+    {
+        $record = new Vehicles([
+            'value' => $request->toArray()['vehicle']
+        ]);
+
+        $record->save();
+        return back();
+    }
+
+    public function edit(Request $request)
+    {
+        Vehicles::where('id',$request->toArray()['id'])
+            ->update([
+                'value' => $request->toArray()['vehicle']
+            ]);
+
+        return back();
+    }
+
+    public function delete(Request $request)
+    {
+        Vehicles::where('id', $request->toArray()['id'])->delete();
+
+        return back();
     }
 }
