84bcce630ef12ea419d9f1460f9f62d23f1f1768
Mantas Gelčius <manciusg@gmail.com>
2017-07-04 23:36:15 +0300

    Improved welcome blade
    Changed blade from default laravel to my credentials

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -69,7 +69,7 @@
             @if (Route::has('login'))
                 <div class="top-right links">
                     @if (Auth::check())
-                        <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{route('travel_data')}}">Home</a>
                     @else
                         <a href="{{ url('/login') }}">Login</a>
                         <a href="{{ url('/register') }}">Register</a>
@@ -79,15 +79,13 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Mantas Gelčius - TravelingSystem
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="https://github.com/SpecoMan/datadog">GitHub</a>
+                    <a href="https://www.facebook.com/mantas.gelcius">Facebook</a>
+                    <a href="https://www.linkedin.com/in/mantas-gel%C4%8Dius-20a2a1146/">Linkedin</a>
                 </div>
             </div>
         </div>
